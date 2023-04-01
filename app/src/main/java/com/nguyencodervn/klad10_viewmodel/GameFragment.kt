c94c20138b345062b0d5f40ffce4f5bf92c55093
NguyenCoderVN <nguyenmanhtriet@gmail.com>
2023-04-01 20:42:25 +0700

    Commit

---
+++ app/src/main/java/com/nguyencodervn/klad10_viewmodel/GameFragment.kt
@@ -0,0 +1,6 @@
+package com.nguyencodervn.klad10_viewmodel
+
+import androidx.fragment.app.Fragment
+
+
+class GameFragment : Fragment(R.layout.fragment_game)
---
+++ app/src/main/java/com/nguyencodervn/klad10_viewmodel/ResultFragment.kt
@@ -0,0 +1,5 @@
+package com.nguyencodervn.klad10_viewmodel
+
+import androidx.fragment.app.Fragment
+
+class ResultFragment : Fragment(R.layout.fragment_result)
