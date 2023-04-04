d8c632675f44f82d011a0cbcca9b5428032983e1
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-04-04 09:39:23 +0700

    commit

--- app/src/main/java/com/nguyencodervn/klad10_guesswordgame/GameFragment.kt
+++ app/src/main/java/com/nguyencodervn/klad10_guesswordgame/GameFragment.kt
@@ -1,7 +1,6 @@
 package com.nguyencodervn.klad10_guesswordgame
 
 import android.os.Bundle
-import android.text.InputFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
 class GameFragment : Fragment(R.layout.fragment_game) {
     private lateinit var displayTv: TextView
@@ -17,13 +15,6 @@ class GameFragment : Fragment(R.layout.fragment_game) {
     private lateinit var wrongTv: TextView
     private lateinit var guessEt: EditText
     private lateinit var guessBt: Button
-
-    private lateinit var words: Array<String>
-    private lateinit var randomWord: String
-    private var display = ""
-    private var live = 8
-    private var wrong = ""
-    private var correct = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,70 +26,15 @@ class GameFragment : Fragment(R.layout.fragment_game) {
             container,
             false
         )
+        initView(view)
+        return view
+    }
 
+    private fun initView(view: View) {
         displayTv = view.findViewById(R.id.displayTv)
         liveTv = view.findViewById(R.id.liveTv)
         wrongTv = view.findViewById(R.id.wrongTv)
         guessEt = view.findViewById(R.id.guessEt)
         guessBt = view.findViewById(R.id.guessBt)
-
-        words = requireActivity().resources.getStringArray(R.array.list_word)
-        randomWord = words.random().uppercase()
-
-        return view
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        updateDisplay()
-        updateUI()
-        guessEt.filters = guessEt.filters + InputFilter.AllCaps()
-        guessBt.setOnClickListener {
-            val letter = guessEt.text
-            if (letter.isNotEmpty() &&
-                !correct.contains(letter) &&
-                !wrong.contains(letter)
-            ) {
-                if (randomWord.contains(letter)) {
-                    correct += letter
-                } else {
-                    wrong += "$letter "
-                    live--
-                }
-                updateDisplay()
-                updateUI()
-            }
-            guessEt.text = null
-
-
-            var transferTxt = ""
-            if (live == 0) {
-                transferTxt = getString(R.string.lost, randomWord)
-            }
-            if (randomWord == display) {
-                transferTxt = getString(R.string.win, randomWord)
-            }
-            if (transferTxt.isNotEmpty()) {
-                val action = GameFragmentDirections
-                    .actionGameFragmentToResultFragment(transferTxt)
-                findNavController().navigate(action)
-            }
-        }
-    }
-
-    private fun updateUI() {
-        displayTv.text = display
-        liveTv.text = getString(R.string.live, live.toString())
-        wrongTv.text = getString(R.string.wrong, wrong)
-    }
-
-    private fun updateDisplay() {
-        var str = ""
-        randomWord.forEach {
-            str += checkLetter(it.toString())
-        }
-        display = str
-    }
-
-    private fun checkLetter(it: String) = if (correct.contains(it)) it else "_"
 }
--- app/src/main/java/com/nguyencodervn/klad10_guesswordgame/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad10_guesswordgame/MainActivity.kt
@@ -1,108 +1,24 @@
 package com.nguyencodervn.klad10_guesswordgame
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
-import androidx.core.os.LocaleListCompat
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.badge.BadgeDrawable
-import com.google.android.material.badge.BadgeUtils
-import com.google.android.material.badge.ExperimentalBadgeUtils
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var materialToolbar: MaterialToolbar
-    private lateinit var enBadge: BadgeDrawable
-    private lateinit var vnBadge: BadgeDrawable
-    private lateinit var defaultLanguage: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        loadLocale()
-        Log.i("MYTAG", "onCreate: ")
         setContentView(R.layout.activity_main)
         materialToolbar = findViewById(R.id.materialToolbar)
         setSupportActionBar(materialToolbar)
     }
 
-
-    @ExperimentalBadgeUtils
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        initBadges()
-        if (defaultLanguage == "en") enBadge.isVisible =
-            true else vnBadge.isVisible = true
-
         menuInflater.inflate(R.menu.menu_option, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
-    @ExperimentalBadgeUtils
-    private fun initBadges() {
-        val blue300 = ContextCompat.getColor(baseContext, R.color.blue_300)
-        enBadge = BadgeDrawable.create(this).apply {
-            isVisible = false
-            backgroundColor = blue300
-        }
-        vnBadge = BadgeDrawable.create(this).apply {
-            isVisible = false
-            backgroundColor = blue300
-        }
-
-        BadgeUtils.attachBadgeDrawable(
-            enBadge,
-            materialToolbar, R.id.englishMn
-        )
-
-        BadgeUtils.attachBadgeDrawable(
-            vnBadge,
-            materialToolbar, R.id.vietnamMn
-        )
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.englishMn -> {
-                enBadge.isVisible = true
-                vnBadge.isVisible = false
-                setLocale("en")
-                recreate()
-            }
-            R.id.vietnamMn -> {
-                enBadge.isVisible = false
-                vnBadge.isVisible = true
-                setLocale("vi")
-                recreate()
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun setLocale(lang: String) {
-
-//        val locale = Locale(lang)
-//        Locale.setDefault(locale)
-//        val config = resources.configuration
-//        config.setLocale(locale)
-//
-//        baseContext.resources.updateConfiguration(
-//            config, baseContext.resources.displayMetrics
-//        )
-
-        val appLocale = LocaleListCompat.forLanguageTags(lang)
-        AppCompatDelegate.setApplicationLocales(appLocale)
-
-        val shared = getSharedPreferences("Setting", MODE_PRIVATE)
-        shared.edit().putString("Lang", lang).apply()
-    }
-
-    private fun loadLocale() {
-        val shared = getSharedPreferences("Setting", MODE_PRIVATE)
-        val lang = shared.getString("Lang", "") ?: ""
-        defaultLanguage = lang
-        setLocale(lang)
-    }
 }
