ea8c82439f1e8ca10a84e5c92a80d3f3a4a90c93
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-04-07 07:41:40 +0700

    Commit

--- app/src/main/java/com/nguyencodervn/klad12_savedstatehandle/GameFragment.kt
+++ app/src/main/java/com/nguyencodervn/klad12_savedstatehandle/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.nguyencodervn.klad12_savedstatehandle.databinding.FragmentGameBinding
 
@@ -30,18 +31,18 @@ class GameFragment : Fragment(R.layout.fragment_game) {
 //    Cách 03
 //     Chú ý thêm implementation 'androidx.fragment:fragment-ktx:1.5.6'
 //     private val viewModel : GameViewModel by viewModels()
-//    private val viewModel: GameViewModel by viewModels {
-//        GameViewModelFactory(7)
-//    }
+    private val viewModel: GameViewModel by viewModels {
+        GameViewModelFactory(3)
+    }
 //     Android ViewModel
 //    private val viewModel: GameAndroidViewModel by viewModels()
 //
 
 //    Cách 04 activityViewModels Nếu bạn muốn ViewModel tồn tại
 //    các Fragment khác nhau trong cùng 1 activity
-    private val viewModel: GameAndroidViewModelWithPara by activityViewModels {
-        GameAndroidViewModelWithParaFactory(3, requireActivity().application)
-    }
+//    private val viewModel: GameAndroidViewModelWithPara by activityViewModels {
+//        GameAndroidViewModelWithParaFactory(3, requireActivity().application)
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
